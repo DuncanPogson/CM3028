0e0457e139012731884ba030ad952400d2f45e44
1405466 <duncanpogson@gmail.com>
2016-11-14 15:27:04 +0000

    creating a file to attempt to link to the database

--- Database/ConnectingToDB.php
+++ Database/ConnectingToDB.php
@@ -5,10 +5,17 @@
  * Time: 15:07
  * Connect to groupD3 web app and select database
  */
-$db = new mysqli(
-    'eu-cdbr-azure-north-e.cloudapp.net'
-    'b4defdb830e2bc'
-    '44f41f1c'
-    'cm3028_groupd3_db'
-);
+//Defining variables to store the database info
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b4defdb830e2bc\'');
+define('DB_PASSWORD', '44f41f1c');
+define('DB_DATABASE', 'cm3028_groupd3_db');
+
+//attempting to connect to the database
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
 //test if connection was established and print any errors
+if($db -> connect_errno){
+    die('ConnectFailed['.$db->connect_error.']');
+}
+
