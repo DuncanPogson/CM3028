28c4cbf7d8eecfa0f3dfbde8920f678f154e3b5e
1405466 <duncanpogson@gmail.com>
2016-11-14 15:49:10 +0000

    Creating a system for logging in
    Began to add the different access levels
    
    Created colour associations with different access levels
    
    Added a simple username and password system
    
    Added a html form which will take in users username and password to hopefully log them in

---
+++ Database/LoginSystem/LoggingIn.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1405466
+ * Date: 14/11/2016
+ * Time: 15:44
+ */
+
+session_start();
+$accessLevel = $_COOKIE['access_level_cookie'];
+
+displayAccessLevelInformation($accessLevel);
+
+function displayAccessLevelInformation($accessLevel)
+{
+    if($accessLevel == "standarduser"){
+        echo "<p style = \"background-colour: lightgreen\">You are currently logged in as a 'Contributor'</p>";
+    }
+    elseif($accessLevel == "root"){
+        echo"<p style = \"background-colour: lightblue\">You are currently logged in as a 'NKPAG' </p>";
+        echo"<p style = \"background-colour: lightblue\">You may now confirm map information</p>";
+    }
+    elseif($accessLevel == "ClubAdmin"){
+        echo"<p style = \"background-colour: lightyellow\">You are currently logged in as a 'Club Admin' </p>";
+        echo"<p style = \"background-colour: lightyellow\">You may now Add/Edit club information/ add Clubs/ Update Own Club</p>";
+    }
+    elseif($accessLevel == "SiteAdmin"){
+        echo"<p style = \"background-colour: lightPink\">You are currently logged in as a 'Site Admin' </p>";
+        echo"<p style = \"background-colour: lightPink\">You may now confirm new users/ Set roles/ edit all information/ add/remove all information</p>";
+    }
+}
