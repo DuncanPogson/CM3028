9cb2dcf5b4c0a0ce850c4fbcc2b7047ae5597562
1405466 <duncanpogson@gmail.com>
2016-11-14 16:31:46 +0000

    changing the passwords input type which will show asterisks when entering a password

--- Database/ConnectingToDB.php
+++ Database/ConnectingToDB.php
@@ -5,29 +5,21 @@
  * Time: 15:07
  * Connect to groupD3 web app and select database
  */
-//Defining variables to store the database info
-//define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
-//define('DB_USERNAME', 'b4defdb830e2bc');
-//define('DB_PASSWORD', '44f41f1c');
-//define('DB_DATABASE', 'cm3028_groupd3_db');
-
+//Storing login info for database as variables
 $serverName = "eu-cdbr-azure-north-e.cloudapp.net";
 $DB_name = "b4defdb830e2bc";
 $DB_password = "44f41f1c";
 $DB = "cm3028_groupd3_db";
 
 //attempting to connect to the database
-//$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
 $conn = new mysqli($serverName, $DB_name, $DB_password, $DB);
 
+//Letting the user know if the connection didnt work
 if($conn->connect_error){
     die('ConnectFailed['.$db->connect_error.']');
 }
+//Letting the user know that the connection was successful
 echo "connected Successfully";
 
-//test if connection was established and print any errors
-//if($db -> connect_errno){
-//    die('ConnectFailed['.$db->connect_error.']');
-//}
+
 
