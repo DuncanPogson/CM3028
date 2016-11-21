0f1f094b71554053f3a16887b1f75ae847bd56bf
1405466 <duncanpogson@gmail.com>
2016-11-21 14:16:13 +0000

    Coding the index page

--- Database/LoginSystem/SignUp.php
+++ Database/LoginSystem/SignUp.php
@@ -6,3 +6,20 @@
  * Time: 16:47
  */
 
+session_start();
+
+//Connect to Database
+include_once 'DB_Connect.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Script</title>
+</head>
+<body>
+    <nav class="navbar navbar-default" role "navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1405466
+ * Date: 21/11/2016
+ * Time: 14:08
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <title>Active Portlethen</title>
+</head>
+<body>
+<header>
+    <h1>
+        The active portlethen homepage
+    </h1>
+</header>
+<p>What would you like to do?</p>
+<ul>
+    <li><a href="Database/LoginSystem/SignUp.php">Sign up to the site</a></li>
+    <li><a href="Database/LoginSystem/loginIndex.html">Log into the site</a></li>
+    
+</ul>
+</body>
