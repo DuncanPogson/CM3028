84aa25267ba4930048a607b0d7b088045f6653dd
1405466 <duncanpogson@gmail.com>
2016-11-28 16:20:06 +0000

    renamed a couple of files and tried a new approach to the login system

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
 <p>What would you like to do?</p>
 <ul>
     <!--Linking to other areas of the site -->
-    <li><a href="Database/LoginSystem/SignUp.html">Sign up to the site</a></li>
-    <li><a href="Database/LoginSystem/loginIndex.html">Log into the site</a></li>
+    <li><a href="Database/Login_SignUp/SignUp.html">Sign up to the site</a></li>
+    <li><a href="Database/Login_SignUp/loginIndex.html">Log into the site</a></li>
     
 </ul>
 </body>
