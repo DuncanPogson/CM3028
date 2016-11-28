531bb5de56d7a7c25b42fb33567d04c6b1085eb8
duncanpogson <duncanpogson@gmail.com>
2016-11-28 21:32:03 +0000

    Re working more of the site

--- health_article.php
+++ health_article.php
@@ -3,24 +3,28 @@
  * Created by PhpStorm.
  * User: duncanpogson
  * Date: 28/11/2016
- * Time: 20:59
+ * Time: 21:25
  */
 include ("/Database/LoginSystem/DB_Connect.php");
 include ("header.php");
+$articleID = $params['blogID'];
 echo "
 <main>
-<h2>Health Articles</h2>
-<ul>
 ";
-$sql = "SELECT * FROM health news ";
+$sql = "SELECT * FROM health news where itemID = '$articleID'";
 $result = $conn->query($sql);
 while($row = $result->fetch_array())
 {
     $articleID = $row['itemID'];
     $articleName = $row['title'];
     $articleAuthor = "SELECT 'username' FROM users WHERE userID = " + $row['userID'];
-    echo "<li><a href='blog/{$articleID}'>{$articleName}</a> by {$articleAuthor}</
-li>";
+    $articleText = $row['content'];
+    echo "
+<atricle>
+ <h2>{$articleName}</h2>
+ <h3>by {$articleAuthor}</h3>
+ {$articleText}
+ </atricle>";
 }
 echo "
 </main>
