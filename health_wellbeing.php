48cd14d1b0317723edb4c910d42a18933d85bf6d
duncanpogson <duncanpogson@gmail.com>
2016-11-28 23:42:48 +0000

    trying to get the authors username

--- health_wellbeing.php
+++ health_wellbeing.php
@@ -21,7 +21,7 @@ while($row = $result->fetch_array())
     $articleName = $row['title'];
     $articleAuthor = $row['userID'];
     //$authNamSql = "SELECT 'username' FROM users WHERE userID ='" . $articleAuthor ."'";
-    $authorName = mysql_query("SELECT 'username' FROM users WHERE userID ='" . $articleAuthor ."'", $conn);
+    $authorName = mysqli_query("SELECT 'username' FROM users WHERE userID ='" . $articleAuthor ."'");
 
     echo "<li><a href='health_wellbeing.php/{$articleID}'>{$articleName}</a> by {$authorName}</li>";
 
