5f2f6b1ac5316dcf13045e0288e73640f901b5bc
1307894 <joe@6bprincesstreet.com>
2016-12-05 12:29:28 +0000

    Adding placeholder sportlethen page

--- sportlethen.php
+++ sportlethen.php
@@ -1,7 +1,28 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: duncanpogson
  * Date: 28/11/2016
- * Time: 22:39
+ * Time: 20:48
  */
+
+//include the header
+include ("header.php");
+
+echo "
+
+<main>
+
+<h2>Sportlethen Page</h2>
+
+<p>Sportlethen CSH is an association of local clubs who are working together to develop safe and fun sport and fitness activities within their local area. 
+Our website is a single access point to find out more about the fantastic sporting opportunities in our area. </p>
+
+<p>Sportlethen launched on the 13th August 2016 at Portlethen Academy, 
+which gave people the opportunity to take part in taster sessions of the different sports they can take up in the local area. </p>
+
+</main>
+";
+//include the footer
+include ("footer.php");
