1b748f849a2e8d57e8212ee2d18955984e36fc9a
1307894 <joe@6bprincesstreet.com>
2016-12-05 10:50:22 +0000

    Adding placeholder sportlethen page

--- header.php
+++ header.php
@@ -21,8 +21,8 @@ session_start();
             <li><a href="home.php">Home</a></li>
             <li><a href="sportlethen.php">Sportlethen</a></li>
             <li><a href="health_wellbeing.php">Health and Wellbeing</a></li>
-            <li><a href="about.php">about</a></li>
-            <li><a href="contact.php">contact</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
             <?
             if (isset($_SESSION['username'])) {
                 echo "<li><a href='logout'>Logout</a></li>";
