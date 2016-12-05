a2b0519cf0e268f17deb69f4574b245ca451c85f
1408996 <pauliusshiu@gmail.com>
2016-12-05 15:47:51 +0000

    login modal

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 
 
 <!-- Carousel
-================================================== -->
+    ================================================== -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -93,22 +93,18 @@
             <div class="container">
                 <div class="carousel-caption">
                     <h1>Portlethen's guide to healthy living and keeping active</h1>
-                    <p>Sign Up or Login</p>
-                    <p><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-                            Sign In
-                        </button></p>
+                    <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                    <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
                 </div>
             </div>
-
         </div>
-
-
         <div class="item">
-            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <img class src="1 Dizzi's World (2).jpg" alt="logo" style="width:304px;height:228px;">
             <div class="container">
                 <div class="carousel-caption">
                     <h1>Another example headline.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p>, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                     <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
                 </div>
             </div>
@@ -123,7 +119,7 @@
                 </div>
             </div>
         </div>
-
+    </div>
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
@@ -132,7 +128,6 @@
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
-        </div>
 </div><!-- /.carousel -->
 <!--MODAL-->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
