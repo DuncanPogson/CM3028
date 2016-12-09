9778d7e1626054ebce0285b340c960f46a3a1991
Dannyrf1995 <dannyrf1995@gmail.com>
2016-12-09 00:02:14 +0000

    Trying out calendar #5  need to get css working

--- calendar_start.php
+++ calendar_start.php
@@ -13,7 +13,7 @@ echo '<div id="calendar_wrap">';
 echo '<div class="title_bar">';
 echo '<div class="previous_month">"</div>';
 echo '<div class="show_month">' . $showmonth . '/' . $showyear . '</div>';
-echo '<div class="next_month"><"</div>';
+echo '<div class="next_month"></div>';
 echo '</div>';
 echo '<div class="week_days">';
 echo '<div class="days_of_week">Sun</div>';
