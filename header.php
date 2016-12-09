10b0f9cc1bca24e863a1f53c476b1b7ead708f52
1405466 <duncanpogson@gmail.com>
2016-12-09 15:24:03 +0000

    fixing problem with navigation

--- header.php
+++ header.php
@@ -18,11 +18,11 @@ session_start();
     <h1>Go Portlethen</h1>
     <nav>
         <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="sportlethen.php">Sportlethen</a></li>
-            <li><a href="health_wellbeing.php">Health and Well-being</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="/home.php">Home</a></li>
+            <li><a href="/sportlethen.php">Sportlethen</a></li>
+            <li><a href="/health_wellbeing.php">Health and Well-being</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
             <?
             if (isset($_SESSION['login_username'])) {
                 echo "<li><a href='logout.php'>Logout</a></li>";
