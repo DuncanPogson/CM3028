a7c5bd868713feefad35c11ba00e7f889f22c9b5
esotcm <pauliusshiu@gmail.com>
2016-12-12 12:12:30 +0000

    clubs

--- clubs/clubs.php
+++ clubs/clubs.php
@@ -124,11 +124,14 @@ include ("../header.php");
 </div><!--container -->
 <!-- /.container -->
 
-<!-- jQuery -->
-<script src="../js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../js/jquery.js"><\/script>')</script>
 <script src="../js/bootstrap.min.js"></script>
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+<script src="../js/holder.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../js/ie10-viewport-bug-workaround.js"></script>
 
 </body>
 
