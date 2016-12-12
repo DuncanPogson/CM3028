33f3629b5c44c8bbe209f903017da6d4e1475dff
esotcm <pauliusshiu@gmail.com>
2016-12-12 11:59:14 +0000

    clubs

--- clubs/clubs.php
+++ clubs/clubs.php
@@ -1,55 +1,99 @@
-
-<!DOCTYPE HTML>
-<link href="http://belekaslol.azurewebsites.net/css/clubs.css" rel="stylesheet">
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-    <title>Club Page</title>
+    <title>Go Portlethen</title>
 
-    <!-- Bootstrap Core CSS -->
+    <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <!-- Custom styles for this template -->
+    <link href="belekaslol.azurewebsites.net/css/carousel.css" rel="stylesheet">
 </head>
+<!-- NAVBAR
+================================================== -->
+<body>
 <?php
-session_start();
-
 include ("../Database/LoginSystem/DB_Connect.php");
 include ("../calendar_start.php");
-include ("../header.php");?>
-<body>
+include ("../header.php");
 
-<!-- Page Content -->
-<div class="container">
+?>
 
-    <!-- Page Header -->
-   <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">
-            </h1>
+
+<!-- Carousel
+    ================================================== -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img class="first-slide" src="images/sportsz.jpg" alt="First slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:cadetblue;">Portlethen's guide to healthy living and keeping active</h1>
+                    <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+                    <p><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                            Sign up today
+                        </button></p>
+                </div>
+            </div>
+        </div>
+        <!-- boom -->
+        <div class="item">
+            <img class src="images/Clubs.jpg" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Explore clubs</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="third-slide" src="images/zen.jpg" alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Health And Wellbeing</h1>
+                    <p>Tips for healthy life</p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- /.row -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div><!-- /.carousel -->
 
-
-
-
-
-    <div class="row">
+<div class="row">
         <div class="col-md-6">
             <div class="well">
 <?
@@ -78,9 +122,8 @@ include ("../header.php");?>
 ?>
         </div>
     </div>
-</div>
-    <hr>
 
+</div><!--container -->
 <!-- /.container -->
 
 <!-- jQuery -->
