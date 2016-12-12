ccaae2f3e4e73caa6e28c524a70beeb0576f22b6
esotcm <pauliusshiu@gmail.com>
2016-12-12 11:53:41 +0000

    clubs

--- clubs/clubs.php
+++ clubs/clubs.php
@@ -63,7 +63,7 @@ include ("../header.php");?>
                 $clubEmail = $row['clubEmail'];
 
                 echo "<ul class='a'>
-<li><a href='club_page.php/?ID={$clubID}'>{$clubName}</a> Contact: {$clubEmail}, Genre: {$clubGenre}. </li>
+<li><a href='../club_page.php/?ID={$clubID}'>{$clubName}</a> Contact: {$clubEmail}, Genre: {$clubGenre}. </li>
                 </ul>";
             }
 
@@ -81,8 +81,6 @@ include ("../header.php");?>
 </div>
     <hr>
 
-
-</div>
 <!-- /.container -->
 
 <!-- jQuery -->
